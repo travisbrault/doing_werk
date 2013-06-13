39e5d28c8cc70afcf3c97bda8315676d282e81af
travisbrault <brault.travis@gmail.com>
2013-06-13 13:38:24 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DoingWerk::Application.config.secret_token = 'e8481b1ccf5f312ea922f8ab27000063479939016b021d7cae317e31a75db772a3d848be18bbe1d1f953d5eaf4455ddd9a83765506a53ff82d9a459f473302e9'
